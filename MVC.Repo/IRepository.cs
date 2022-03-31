a084ca9670cdded4fc5dae17126ccb42b3ab134e
corion <corion.pradhan@nestrait.com>
2022-03-31 11:21:10 +0545

    Onion Architecture setup

---
+++ MVC.Repo/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVC.Repo
+{
+    public interface IRepository<T> where T : class
+    {
+        //Get all records
+        IEnumerable<T> GetAll();
+
+        //Get by particular ID
+        T GetById(long id);
+
+        //Creating/inserting record into database
+        void Insert(T entity);
+
+        //Editing the record
+        void Update(T entity);
+
+        //Deleting the record
+        void Delete(T entity);
+
+        //Saving the record/changes to database
+        void SaveChanges();
+
+    }
+}
