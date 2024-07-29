8df17ab84eef86b77c71692e3ec41b5c5bacaf8e
Mahesh Singh Madai <maheshmadai977@gmail.com>
2024-07-29 11:31:12 +0545

    upgrade to .net 6

---
+++ MVCApp/Hub/CategoryHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace MVCApp.Hub
+{
+    public class CategoryHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task SendCategoryUpdate(string  categoryName, int orderQuantity)
+        {
+            await Clients.All.SendAsync("ReceiveCategoryUpdate", categoryName, orderQuantity);
+        }
+    }
+}
--- MVCApp/Startup.cs
+++ MVCApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MVCApp.Data;
+using MVCApp.Hub;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,7 @@ namespace MVCApp
 
             );
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +60,7 @@ namespace MVCApp
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<CategoryHub>("/categoryHub");
             });
         }
     }
